0b148778f13086b9acdaf731db70b2fa128f18fd
CloudedSword <granthib00@gmail.com>
2023-01-10 10:56:32 -0700

    Prep assignments 3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("Enter Magic Number: ");
+        //string userMagicNumber = Console.ReadLine();
+        //int magicNumber = int.Parse(userMagicNumber);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = 0;
+        int timesGuessed = 0;
+
+        do
+        {
+            Console.Write("Enter your guess: ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+            timesGuessed += 1;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Too Low!");
+            }
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Too High!");
+            }
+
+            else
+            {
+                Console.WriteLine("You Got it!");
+            }
+
+
+        } while (magicNumber != guess);
+
+        Console.WriteLine($"You guessed {timesGuessed} times!");
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a series of numbers, type zero to finish.");
+        int inputNumber = -1;
+        int sum = 0;
+        int largestNumber = 0;
+
+        do
+        {
+            Console.Write("Enter Number: ");
+            inputNumber = int.Parse(Console.ReadLine());
+            numbers.Add(inputNumber);
+        } while(inputNumber != 0);
+        
+        numbers.Remove(0);
+        int length = numbers.Count;
+        //Console.WriteLine(length);
+
+        foreach (int unit in numbers)
+        {
+            Console.WriteLine(unit);
+            sum += unit;
+            if (unit > largestNumber)
+            {
+                largestNumber = unit;
+            }
+        }
+        
+        float average = (float)sum / length;
+
+        Console.WriteLine(sum);
+        Console.WriteLine(largestNumber);
+        Console.WriteLine(average);
     }
 }
