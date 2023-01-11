2735ace81c89936576e4bb7b10f3f31b0f517f7c
CloudedSword <granthib00@gmail.com>
2023-01-11 11:54:27 -0700

    RE-submit to change function names to Proper case

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -11,32 +11,32 @@ class Program
         displayResults(userName, numberSquared);
     }
 
-    static void displayWelcome()
+    static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
     }
 
-    static string promptUserName()
+    static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         string userName = Console.ReadLine();
         return userName;
     }
 
-    static int favoriteNumber()
+    static int FavoriteNumber()
     {
         Console.Write("Please enter your favorite number: ");
         int number = int.Parse(Console.ReadLine());
         return number;
     }
 
-    static int squareNumber(int number)
+    static int SquareNumber(int number)
     {
         int squaredNumber = number * number;
         return squaredNumber;
     }
 
-    static void displayResults(string userName, int numberSquared)
+    static void DisplayResults(string userName, int numberSquared)
     {
         Console.WriteLine($"{userName}, the square of your number is {numberSquared}.");
     }
